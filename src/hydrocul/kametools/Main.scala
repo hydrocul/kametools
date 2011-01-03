e0d8427dc299fed6d1c85eaf5005eab374d794e3
hydrocul <hydrocul@gmail.com>
2011-01-04 01:20:26 +0900

    create Main.scala

---
+++ src/hydrocul/kametools/Main.scala
@@ -0,0 +1,21 @@
+package hydrocul.kametools;
+
+object Main {
+
+  def main(args: Array[String]){
+    if(args.size == 0){
+      printHelp();
+    } else {
+      args(0) match {
+        case "now" =>
+          now.Now.main(args.drop(1));
+        case cmd =>
+          println("Unknown command: " + cmd);
+      }
+    }
+  }
+
+  def printHelp(){
+  }
+
+}
---
+++ src/hydrocul/kametools/now/Now.scala
@@ -0,0 +1,8 @@
+package hydrocul.kametools.now;
+
+object Now {
+
+  def main(args: Array[String]){
+  }
+
+}
