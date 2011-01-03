5c9a3e6b07d9091b44ceea50a582c25cbb180b66
hydrocul <hydrocul@gmail.com>
2011-01-04 01:02:23 +0900

    create make.sh

---
+++ make.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [ ! -f buildlib.tmp ] ; then
+wget --no-check-certificate https://github.com/hydrocul/kamebuild/raw/master/buildlib.sh
+mv buildlib.sh buildlib.tmp
+fi
+
+. buildlib.tmp
+
+
+
+case $1 in
+################################
+compile)
+################################
+buildlib_mkdir_lib
+if [ $? -ne 0 ] ; then
+  buildlib_dl_commons_cli
+fi
+
+buildlib_compile
+
+if [ $? -ne 0 ] ; then
+exit 1
+fi
+
+. make.sh test
+
+;;
+################################
+test)
+################################
+buildlib_test
+
+;;
+################################
+build)
+################################
+buildlib_build_jar hydrocul-kametools.jar
+
+;;
+################################
+scaladoc)
+################################
+buildlib_scaladoc
+
+;;
+################################
+*)
+################################
+echo "Usage: ./make.sh {compile|test|build|scaladoc}"
+
+;;
+################################
+esac
+
+
+
