d15c85ffaf37ebd0194d3f8453f75610b654ef33
hydrocul <hydrocul@gmail.com>
2011-02-09 00:04:12 +0900

    set system properties in main method about logging

--- src/hydrocul/kametools/Main.scala
+++ src/hydrocul/kametools/Main.scala
@@ -18,6 +18,13 @@ object Main {
 
   def main(args: Array[String]){
 
+//    System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+    System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
+    System.setProperty("org.apache.commons.logging.simplelog.showlogname", "true");
+    System.setProperty("org.apache.commons.logging.simplelog.showShortLogname", "false");
+    System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
+    System.setProperty("org.apache.commons.logging.simplelog.defaultlog", "warn");
+
     if(System.getProperty("kt.platform")==null){
       if(File.separatorChar=='\\'){
         System.setProperty("kt.platform", "Windows");
