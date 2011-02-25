80de72fd7b568c634a54e84d312413e1993b3f97
hydrocul <hydrocul@gmail.com>
2011-02-26 08:59:26 +0900

    move WebBrowser function to WebSite

--- src/hydrocul/kametools/web/WebBrowserApp.scala
+++ src/hydrocul/kametools/web/WebBrowserApp.scala
@@ -12,43 +12,30 @@ import com.gargoylesoftware.htmlunit.util.WebResponseWrapper;
 import hydrocul.kametools.App;
 import hydrocul.kametools.ObjectBank;
 
-case class WebBrowserApp(url: String, mode: WebBrowserApp.OutputMode) extends App {
+case class WebBrowserApp(url: String, query: Option[String]) extends App {
 
   override def exec(env: App.Env){
 
-    val browser = new WebBrowser();
-
-    val page = browser.open(url);
-
-    mode match {
-      case WebBrowserApp.TextMode =>
-        val page2 = page.useObjectBank;
-        env.out.println(page2.getContent);
-      case WebBrowserApp.XmlMode =>
-        env.out.println(page.getXmlSource);
-      case WebBrowserApp.SourceMode =>
-        env.out.println(page.getSource);
+    val q = query match {
+      case Some(q) => q;
+      case None => "";
     }
+
+    val ua = new WebClient();
+    val result = WebSite.list.find(_.matchUrl(url)).get.open(ua, url, q);
+
+    env.out.println(result);
 
   }
 
   override def next(arg: String): App = (nextCommonly(arg), arg) match {
     case (Some(app), _) => app;
-    case (None, "--xml") => WebBrowserApp(url, WebBrowserApp.XmlMode);
-    case (None, "--source") => WebBrowserApp(url, WebBrowserApp.SourceMode);
+    case (None, newQuery) if(!query.isDefined) => WebBrowserApp(url, Some(newQuery));
     case _ => throw new Exception("Unknown option: " + arg);
   }
 
 }
 
 object WebBrowserApp {
-
-  abstract sealed class OutputMode extends java.io.Serializable;
-
-  object TextMode extends OutputMode;
-
-  object XmlMode extends OutputMode;
-
-  object SourceMode extends OutputMode;
 
 }
--- src/hydrocul/kametools/web/WebSite.scala
+++ src/hydrocul/kametools/web/WebSite.scala
@@ -1,27 +1,53 @@
 package hydrocul.kametools.web;
 
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.ThreadedRefreshHandler;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebConnection;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.WebRequest;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.util.WebConnectionWrapper;
+import com.gargoylesoftware.htmlunit.util.WebResponseWrapper;
 
 import hydrocul.kametools.ObjectBank;
 
-trait WebSite {
+class WebSite {
 
-  def urlMatch(url: String): Boolean = true;
+  def matchUrl(url: String): Boolean = true;
 
-  def isJavaScriptEnabled: Boolean = false;
+  def open(ua: WebClient, url: String, query: String): Any = {
 
-  def createPage(html: HtmlPage): WebPage = new WebPage(){
+    ua.setRefreshHandler(new ThreadedRefreshHandler(){
+      override def handleRefresh(page: Page, url: java.net.URL, requestedWait: Int){
+        println(url);
+        super.handleRefresh(page, url, requestedWait);
+      }
+    });
 
-    override def getTitle = html.getTitleText;
+    ua.setJavaScriptEnabled(isJavaScriptEnabled);
 
-    override def getContent = html.asText;
+    val html: Page = ua.getPage(url);
+    if(isJavaScriptEnabled){
+      ua.waitForBackgroundJavaScript(1000);
+    }
 
-    override def getXmlSource = html.asXml;
+    html match {
+      case html: HtmlPage => query match {
+        case "" => html.asText;
+        case "--text" => html.asText;
+        case "--xml" => html.asXml;
+        case "--source" => html.getWebResponse.getContentAsString;
+        case _ => doQuery(html, query);
+      }
+    }
 
-    override def getSource = html.getWebResponse.getContentAsString;
+  }
 
-    override def useObjectBank(ob: ObjectBank): WebPage = this;
+  protected def isJavaScriptEnabled: Boolean = false;
 
+  protected def doQuery(html: HtmlPage, query: String): Any = {
+    throw new Exception("Unknown query: " + query);
   }
 
 }
@@ -33,3 +59,19 @@ object WebSite {
   def list = default :: Nil;
 
 }
+
+/*
+  private def getWebConnection(src: WebConnection): WebConnection = {
+    new WebConnectionWrapper(src){
+      override def getResponse(request: WebRequest): WebResponse = {
+        val res = super.getResponse(request);
+        request.getUrl.toString match {
+          case _ => res;
+        }
+//        new WebResponseWrapper(super.getResponse(request)){
+//        }
+      }
+    }
+  }
+*/
+
