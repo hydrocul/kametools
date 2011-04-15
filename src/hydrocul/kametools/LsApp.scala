4e35464352cdeef6b6d8107b03e048bbca3d9215
hydrocul <hydrocul@gmail.com>
2011-04-15 20:45:35 +0900

    update LsApp

--- src/hydrocul/kametools/LsApp.scala
+++ src/hydrocul/kametools/LsApp.scala
@@ -53,45 +53,6 @@ case class LsApp(fileSet: FileSet, count: Int = 50,
 
   }
 
-/*
-  override def modify(arg: String): Option[App] = {
-    arg match {
-      case "-t" => Some(App.NeedOfArgumentApp(
-        format => LsApp(fileSet, count, format, lineFormat)));
-      case "-f" => Some(App.NeedOfArgumentApp(
-        format => LsApp(fileSet, count, timeFormat, format)));
-      case LsApp.OptionCPattern(c) => Some(LsApp(fileSet,
-        c.toInt, timeFormat, lineFormat));
-      case "-a" => Some(LsApp(fileSet, 0, timeFormat, lineFormat));
-      case "-r" => Some(LsApp(
-        FileSet.recursive(fileSet, 0, -1, false, false),
-        count, timeFormat, lineFormat));
-      case LsApp.OptionRPattern1(d) => Some(LsApp(
-        FileSet.recursive(fileSet, 0, d.toInt, false, false),
-        count, timeFormat, lineFormat));
-      case LsApp.OptionRPattern2(d1, d2) => Some(LsApp(
-        FileSet.recursive(fileSet, d1.toInt, d2.toInt, false, false),
-        count, timeFormat, lineFormat));
-      case LsApp.OptionRPattern3(d1) => Some(LsApp(
-        FileSet.recursive(fileSet, d1.toInt, -1, false, false),
-        count, timeFormat, lineFormat));
-      case "-v" => Some(LsApp(fileSet.reverse,
-        count, timeFormat, lineFormat));
-      case "-p" => Some(App.NeedOfArgumentApp(
-        pattern => LsApp(fileSet.filter(cond(_, pattern)),
-        count, timeFormat, lineFormat)));
-      case "-o" => Some(OpenApp(fileSet));
-      case _ => None;
-    }
-  }
-
-  private def cond(file: File, pattern: String): Boolean = {
-    val name = file.getName;
-    val patterns = pattern.split(" +");
-    !patterns.exists(p => name.indexOf(p) < 0);
-  }
-*/
-
 }
 
 object LsApp {
@@ -104,15 +65,38 @@ object LsApp {
       app;
     } else {
       args match {
-        case "-t" :: format :: tail => create(LsApp(app.fileSet, app.count, format, app.lineFormat), tail);
-        case "-f" :: format :: tail => create(LsApp(app.fileSet, app.count, app.timeFormat, format), tail);
-        case "-a" :: tail => create(LsApp(app.fileSet, 0, app.timeFormat, app.lineFormat), tail);
-作りかけ
+        case "-t" :: format :: tail => create(LsApp(app.fileSet, app.count,
+          format, app.lineFormat), tail);
+        case "-f" :: format :: tail => create(LsApp(app.fileSet, app.count,
+          app.timeFormat, format), tail);
+        case "-a" :: tail => create(LsApp(app.fileSet, 0,
+          app.timeFormat, app.lineFormat), tail);
+        case "-r" :: tail => create(LsApp(FileSet.recursive(app.fileSet, 0, -1,
+          false, false), app.count, app.timeFormat, app.lineFormat), tail);
+        case OptionCPattern(d) :: tail => create(LsApp(app.fileSet, d.toInt,
+          app.timeFormat, app.lineFormat), tail);
+        case OptionRPattern1(d1) :: tail => create(LsApp(FileSet.recursive(app.fileSet,
+          0, d1.toInt, false, false), app.count,
+          app.timeFormat, app.lineFormat), tail);
+        case OptionRPattern2(d1, d2) :: tail => create(LsApp(FileSet.recursive(app.fileSet,
+          d1.toInt, d2.toInt, false, false), app.count,
+          app.timeFormat, app.lineFormat), tail);
+        case OptionRPattern3(d1) :: tail => create(LsApp(FileSet.recursive(app.fileSet,
+          d1.toInt, -1, false, false), app.count,
+          app.timeFormat, app.lineFormat), tail);
+        case "-p" :: pattern :: tail => create(LsApp(app.fileSet.filter(cond(_, pattern)),
+          app.count, app.timeFormat, app.lineFormat), tail);
+        case _ => throw new Exception("Unknown argument: " + args);
       }
     }
   }
 
-/*
+  private def cond(file: File, pattern: String): Boolean = {
+    val name = file.getName;
+    val patterns = pattern.split(" +");
+    !patterns.exists(p => name.indexOf(p) < 0);
+  }
+
   private val OptionCPattern = "-c(\\d+)".r;
 
   private val OptionRPattern1 = "-r-?(\\d+)".r;
@@ -120,7 +104,6 @@ object LsApp {
   private val OptionRPattern2 = "-r(\\d+)-(\\d+)".r;
 
   private val OptionRPattern3 = "-r(\\d+)-".r;
-*/
 
 }
 
