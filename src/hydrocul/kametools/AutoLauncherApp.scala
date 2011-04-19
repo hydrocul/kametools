48669c8988910386dc5d34a189e61c9c3785bfdf
hydrocul <hydrocul@gmail.com>
2011-04-19 14:49:41 +0900

    create AutoLauncherApp

--- src/hydrocul/kametools/AutoLauncherApp.scala
+++ src/hydrocul/kametools/AutoLauncherApp.scala
@@ -1,27 +1,27 @@
 package hydrocul.kametools;
 
+import java.io.File;
+
 case class AutoLauncher(downloadDir: String) extends App {
 
   override def exec(env: App.Env){
+    var files = getExistingFiles();
     while(true){
-      waitSub(env);
+      files = waitSub(env, files);
       Thread.sleep(500);
     }
   }
 
-  private def waitSub(env: App.Env){
-/*
-    val fileNames = (new File(downloadDir)).listFiles.toSeq.flatMap(_ match {
-      case pattern(n1, n2) => Some(n1 + n2);
-      case _ => None;
-    });
-    println(fileNames);
-*/
+  private def waitSub(env: App.Env, files: List[File]): List[File] = {
+    val newFiles = getExistingFiles();
+    val openingFiles = newFiles.diff(files);
+    val openingFile = openingFiles.head;
+    OpenApp.openDefault(openingFile, env);
+    openingFile :: files;
   }
 
-/*
-  private val pattern = "(.*)-ktopen(.[^.]+)".r;
-*/
+  private def getExistingFiles(): List[File] =
+    (new File(downloadDir)).listFiles().toList;
 
 }
 
