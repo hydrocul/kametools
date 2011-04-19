1d4e4ecc371a9df93c98356548159cdac00fa163
hydrocul <hydrocul@gmail.com>
2011-04-19 23:00:17 +0900

    update PutApp to change the remote file name rule

--- src/hydrocul/kametools/PutApp.scala
+++ src/hydrocul/kametools/PutApp.scala
@@ -7,11 +7,7 @@ case class PutApp(fileSet: FileSet, target: SyncTarget) extends App {
   override def exec(env: App.Env){
     val file = fileSet.head;
     val name = file.getName();
-    val i = name.lastIndexOf('.');
-    val nameOnTarget = if(i < 0)
-      ObjectBank.default.put(fileSet);
-    else
-      ObjectBank.default.put(fileSet) + name.substring(i);
+    val nameOnTarget = createRemoteFileName(name);
     val syncFile = SyncFile(file, target, nameOnTarget);
     val syncFiles = ObjectBank.default.getOrElse("_syncFiles", Vector.empty[SyncFile]);
     val newSyncFiles = syncFiles :+ syncFile;
@@ -21,6 +17,21 @@ case class PutApp(fileSet: FileSet, target: SyncTarget) extends App {
       " " + App.escapeFilePath(file.getAbsolutePath) +
       " " + App.escapeFilePath(target.targetDir + nameOnTarget);
     env.shellScriptWriter.println(script);
+  }
+
+  private def createRemoteFileName(name: String): String = {
+    val ret = new StringBuilder();
+    (1 to 4).foreach { _ =>
+      val r = (math.random * 26).asInstanceOf[Int];
+      ret.append(('a' + r).asInstanceOf[Char]);
+    }
+    ret.append((new java.util.Date()).getTime);
+    val Pattern = ".*(\\.[a-zA-Z0-9]+)".r;
+    name match {
+      case Pattern(ext) => ret.append(ext);
+      case _ => ;
+    }
+    ret.toString;
   }
 
 }
